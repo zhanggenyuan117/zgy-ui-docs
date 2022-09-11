ec2f047f202d902c8306aef3ada64277114c4211
zhanggenyuan117 <834390901@qq.com>
2022-09-11 21:42:31 +0800

    docs/.vitepress/dist

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,7 +1,7 @@
 /*
  * @Author: zhang_gen_yuan
  * @Date: 2022-09-09 23:24:05
- * @LastEditTime: 2022-09-11 21:26:07
+ * @LastEditTime: 2022-09-11 21:42:01
  * @Descripttion:
  */
 
@@ -18,6 +18,7 @@ export default {
       },
       lineNumbers: true,
     },
+    base:'/docs/.vitepress/dist/',
     nav: [
       { text: "指南", link: "/guild/installation" },
       { text: "组件", link: "/examples/button/" },
---
+++ docs/.vitepress/dist/assets/guild_quickstart.md.066c1fd8.js
@@ -0,0 +1,10 @@
+import{_ as s,c as a,o as n,a as t}from"./app.8fbc350f.js";const C=JSON.parse('{"title":"\u5FEB\u901F\u5F00\u59CB","description":"","frontmatter":{},"headers":[{"level":2,"title":"\u7528\u6CD5","slug":"\u7528\u6CD5","link":"#\u7528\u6CD5","children":[]}],"relativePath":"guild/quickstart.md"}'),e={name:"guild/quickstart.md"},l=t(`<h1 id="\u5FEB\u901F\u5F00\u59CB" tabindex="-1">\u5FEB\u901F\u5F00\u59CB <a class="header-anchor" href="#\u5FEB\u901F\u5F00\u59CB" aria-hidden="true">#</a></h1><p>\u672C\u8282\u5C06\u4ECB\u7ECD\u5982\u4F55\u5728\u9879\u76EE\u4E2D\u4F7F\u7528 zgyUI</p><h2 id="\u7528\u6CD5" tabindex="-1">\u7528\u6CD5 <a class="header-anchor" href="#\u7528\u6CD5" aria-hidden="true">#</a></h2><div class="language-"><button class="copy"></button><span class="lang"></span><pre><code><span class="line"><span style="color:#A6ACCD;">&lt;template&gt;</span></span>
+<span class="line"><span style="color:#A6ACCD;">  &lt;Button&gt;\u6309\u94AE&lt;/Button&gt;</span></span>
+<span class="line"><span style="color:#A6ACCD;">&lt;/template&gt;</span></span>
+<span class="line"><span style="color:#A6ACCD;"></span></span>
+<span class="line"><span style="color:#A6ACCD;">&lt;script setup lang=&quot;ts&quot;&gt;</span></span>
+<span class="line"><span style="color:#A6ACCD;"></span></span>
+<span class="line"><span style="color:#A6ACCD;">import { Button } from &#39;zgy-ui&#39;</span></span>
+<span class="line"><span style="color:#A6ACCD;"></span></span>
+<span class="line"><span style="color:#A6ACCD;">&lt;/script&gt;</span></span>
+<span class="line"><span style="color:#A6ACCD;"></span></span></code></pre></div>`,4),p=[l];function o(c,r,i,d,_,u){return n(),a("div",null,p)}const h=s(e,[["render",o]]);export{C as __pageData,h as default};
