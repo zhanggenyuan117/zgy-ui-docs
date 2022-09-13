d23ee31550eb86128ea883cedad064c6dd46dca4
zhanggenyuan <834390901@qq.com>
2022-09-13 15:48:54 +0800

    ddd

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -18,7 +18,7 @@ export default {
       },
       lineNumbers: true,
     },
-    base:'./',
+    base:'/zgy-ui-docs/docs/.vitepress/dist/',
     nav: [
       { text: "指南", link: "/guild/installation" },
       { text: "组件", link: "/examples/button/" },
